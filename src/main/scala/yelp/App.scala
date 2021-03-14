169efbfdfa05fecb1dd5b3f65e318a088ae532a0
jinyung2 <jinyoungchoi118@gmail.com>
2021-03-14 01:31:09 -0800

    cleaned up proj directory, added stuff to gitignore

--- src/main/scala/yelp/App.scala
+++ src/main/scala/yelp/App.scala
@@ -2,15 +2,26 @@ package yelp
 
 import org.apache.spark.SparkContext._
 import scala.io._
-import org.apache.spark.SparkConf
-import org.apache.spark.SparkContext
+import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.rdd._
 import org.apache.log4j.Logger
 import org.apache.log4j.Level
 import scala.collection._
 
 object App {
-  def main(args: Array[String]) = {
 
+  val INPUT_FILE = "script/yelp_review_trimmed.json"
+  val TEST_INPUT = "test_output.txt"
+
+  def main(args: Array[String]) = {
+    Logger.getLogger("org").setLevel(Level.OFF)
+    Logger.getLogger("akka").setLevel(Level.OFF)
+
+    val conf = new SparkConf().setAppName("NameOfApp")
+      .setMaster("local[4]")
+    val sc = new SparkContext(conf)
+
+    val reviews = sc.textFile(TEST_INPUT).persist()
+      .collect().foreach(println)
   }
 }
